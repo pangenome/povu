e1b8c38e8c682c0a5dfc1603ad80886d1d281183
Erik Garrison <erik.garrison@gmail.com>
2023-03-22 18:12:20 +0100

    it broken

--- domibubble.py
+++ domibubble.py
@@ -54,70 +54,92 @@ def break_cycles_tarjan(G):
     return new_G
 
 def dominator_tree(G, entry):
+    # Initialize data structures for the algorithm
     semi = defaultdict(set)
     idom = {}
     s = {}
     bucket = defaultdict(list)
 
+    # Depth-first search to populate 'semi' and 's' dictionaries
     def dfs(n):
-        print("dfs", n)
         if n not in semi:
             semi[n] = len(semi)
             s[semi[n]] = n
             for m in G.successors(n):
                 dfs(m)
 
+    # Function to find the ancestor of 'v' with the lowest semi value
     def ancestor_with_lowest_semi(v):
-        print("ancestor_with_lowest_semi", v)
+        if v not in parent:
+            return None
         u = v
-        while v in s:
+        while v in parent:
             if semi[v] < semi[u]:
                 u = v
-            v = parent[v]
+            v = parent.get(v, None)
         return u
 
+    # Function to set the parent of 'w' to 'v'
     def link(v, w):
         parent[w] = v
 
+    # Perform the depth-first search starting from the entry node
     dfs(entry)
     n = len(semi) - 1
     parent = {}
 
+    # Iterate through nodes in reverse order of the depth-first search
     while n > 0:
         w = s[n]
+        
+        # Calculate semi values for 'w' by iterating through predecessors of 'w'
         for v in G.predecessors(w):
             u = ancestor_with_lowest_semi(v)
-            if semi[u] < semi[w]:
+            if u is not None and semi[u] < semi[w]:
                 semi[w] = semi[u]
 
+        # Add 'w' to the bucket of nodes with the same semi value
         bucket[s[semi[w]]].append(w)
 
+        # Process nodes in the bucket of the parent of 'w'
         while bucket[s[n - 1]]:
             v = bucket[s[n - 1]].pop()
             u = ancestor_with_lowest_semi(v)
-            idom[v] = u if semi[u] < semi[v] else s[n - 1]
+            if u is not None:
+                # Set the immediate dominator of 'v'
+                idom[v] = u if semi[u] < semi[v] else s[n - 1]
 
         n -= 1
 
+    # Update the immediate dominator values for all nodes
     for n in range(1, len(s)):
+        print("n: ", n)
+        print("s[n]: ", s[n])
         w = s[n]
-        if idom[w] != s[semi[w]]:
+        if w in idom and idom[w] != s[semi[w]]:
             idom[w] = idom[idom[w]]
 
-    return idom
+    # Build the dominator tree as a NetworkX DiGraph using the calculated idom values
+    dom_tree = nx.DiGraph()
+    for node, dominator in idom.items():
+        if dominator is not None:
+            dom_tree.add_edge(dominator, node)
 
+    return dom_tree
+ 
 def find_bubbles(G, entry):
     G = break_cycles_tarjan(G)
-    nx.drawing.nx_pydot.write_dot(G, 'broken.dot')
-    # run dot to create a png
-    subprocess.call(['dot', '-Tpng', 'broken.dot', '-o', 'broken.png'])
     topological_order = list(nx.topological_sort(G))
     dom_tree = dominator_tree(G, entry)
     bubbles = []
-
-    for parent, child in dom_tree.items():
+    # write the dominator tree to a dot file
+    nx.drawing.nx_pydot.write_dot(dom_tree, 'dom_tree.dot')
+    # use dot to convert the dot file to a png file
+    subprocess.call(['dot', '-Tpng', 'dom_tree.dot', '-o', 'dom_tree.png'])
+    for parent in dom_tree:
         if G.in_degree(parent) > 1:
-            max_child_order = max(topological_order.index(c) for c in G.successors(parent))
+            children = list(dom_tree.successors(parent))
+            max_child_order = max(topological_order.index(c) for c in children)
             bubble_end = topological_order[max_child_order]
             bubbles.append((parent, bubble_end))
 
