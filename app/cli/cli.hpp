d9250c8becb1301cc757853b4009e67253f13afe
Njagi Mwaniki <njagi@urbanslug.com>
2025-11-11 16:51:37 +0100

    out of sync version bump commit to 0.0.1

--- app/cli/cli.hpp
+++ app/cli/cli.hpp
@@ -1,15 +1,13 @@
 #ifndef PV_CLI_HPP
 #define PV_CLI_HPP
 
-#include <string> // for basic_string, string
+#include <string_view> // for string_view
 
 #include "povu/common/app.hpp" // for config
 
 namespace cli
 {
-
-// app version string constant
-const std::string VERSION = "0.0.0-alpha";
+constexpr std::string_view VERSION = "0.0.1-alpha"; // app & lib version
 
 int cli(int argc, char **argv, core::config &app_config);
 } // namespace cli
