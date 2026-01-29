dab9aac2b72db7a6333d346ac93ece4c1eea60f6
Njagi Mwaniki <njagi@urbanslug.com>
2026-01-29 12:23:01 +0100

    track povu refs implementation file

---
+++ src/povu/refs/refs.cpp
@@ -0,0 +1,11 @@
+#include <liteseq/gfa.h>
+
+namespace povu::refs
+{
+
+char lq_strand_to_char(liteseq::strand s)
+{
+	return (s == liteseq::strand::STRAND_FWD) ? '>' : '<';
+}
+
+}; // namespace povu::refs
